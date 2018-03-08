5833f53959086113a68d840852486ff6ba0a42d1
skyhawk33 <ryleyj@shaw.ca>
2018-03-08 15:55:19 -0800

    Implemented view

--- src/java/Model.java
+++ src/java/Model.java
@@ -7,49 +7,134 @@ import java.util.logging.*;
 
 public class Model extends GridWorldModel
 {
-	public static final int VIRUS_CARD = 1 << 3;
-	public static final int ANTI_VIRUS_CARD = 1 << 4;
+	//constants ---------------------------------------
+	
+	public static final int TABLE = 1 << 3;
+	public static final int BOARD = 1 << 4;
+	public static final int VIRUS_CARD = 1 << 5;
+	public static final int ANTI_VIRUS_CARD = 1 << 6;
+	
+	public static final int VIRUS_ROLE = 0;
+	public static final int ROGUE_ROLE = 1;
+	public static final int ANTI_VIRUS_ROLE = 2;
+	
+	public static final int NUM_PLAYERS = 6;
 	
 	private static final int GRID_WIDTH = 13;
 	private static final int GRID_HEIGHT = 13;
 	
 	private static final int VIRUS_CARDS = 11;
 	private static final int ANTI_VIRUS_CARDS = 6;
+	private static final int MAX_HAND = 3;
 	
-	private static final int NUM_PLAYERS = 6;
+	private static final int NO_ABILITY = 0;
+	private static final int CHECK_STACK_3 = 1;
+	private static final int INSPECT_ELEMENT = 2;
+	private static final int ASSIGN_KERNEL = 3;
+	private static final int DELETE_AGENT = 4;
+	
+	private static final int[] BOARD5_6 =	{0,0,1,4,4,0};
+	private static final int[] BOARD7_8 =	{0,2,3,4,4,0};
+	private static final int[] BOARD9_10 =	{2,2,3,4,4,0};
+	
+	//variables ---------------------------------------
 	
 	private int numVirusCards;
 	private int numAntiVirusCards;
-	List<Integer> deck;
-	List<Integer> discard;
-	List<Integer>[] hand;
+	private List<Integer> deck;
+	private List<Integer> discard;
+	private List<Integer>[] hand;
+	
+	private int[] role;
+	private int[] board;
 	
 	private int kernelID = -1;
 	private int schedulerID = -1;
 	private int exKernelID = -1;
 	private int exSchedulerID = -1;
 	
+	//initialization ----------------------------------
+	
 	public Model()
 	{
 		super(GRID_WIDTH, GRID_HEIGHT, NUM_PLAYERS);
-		//agents sit in static locations
-		setAgPos(0, 2, 2);
-		setAgPos(1, 6, 2);
-		setAgPos(2, 10, 2);
-		setAgPos(3, 2, 10);
-		setAgPos(4, 6, 10);
-		setAgPos(5, 10, 10);
+		//set up the board
+		if(NUM_PLAYERS < 7)
+			board = Arrays.copyOf(BOARD5_6,BOARD5_6.length);
+		else if(NUM_PLAYERS < 9)
+			board = Arrays.copyOf(BOARD7_8,BOARD7_8.length);
+		else
+			board = Arrays.copyOf(BOARD9_10,BOARD9_10.length);
+		//set up view positions
+		initPositions();
 		//initially no cards are played
 		numVirusCards = 0;
 		numAntiVirusCards = 0;
 		//set up the deck
 		initDecks();
+		//set up roles
+		initRoles();
 		//set up the hands
 		hand = new List[NUM_PLAYERS];
 		for(int i = 0; i < NUM_PLAYERS; i++)
-			hand[i] = new ArrayList<>(3);
+			hand[i] = new ArrayList<>(MAX_HAND);
 		//set the kernel
-		kernelID = 0;
+		kernelID = random.nextInt(NUM_PLAYERS);
+	}
+	
+	private void initPositions()
+	{
+		//ternary operators are cool.
+		//spacing changes depending on number of players
+		int spacing = NUM_PLAYERS > 6 ? 2 : 4;
+		int dx = 0;
+		int dy = 0;
+		//agents sit in static locations
+		for(int i = 0; i < NUM_PLAYERS; i++){
+			dx = (i < NUM_PLAYERS/2) ?
+					spacing*i:
+					spacing*(i-NUM_PLAYERS/2);
+			dy = (i < NUM_PLAYERS/2) ?
+					0:
+					8;
+			setAgPos(i, 2+dx, 2+dy);
+		}
+		for(dx = 0; dx < 9; dx++)
+			for(dy = 0; dy < 5; dy++)
+				set(TABLE, 2+dx, 4+dy);
+		for(int i = 0; i < 6; i++)
+			set(BOARD, 3+i, 5);
+		for(int i = 0; i < 5; i++)
+			set(BOARD, 3+i, 7);
+	}
+	
+	private void initRoles()
+	{
+		List<Integer> tempRole = new ArrayList<>(NUM_PLAYERS);
+		
+		if(NUM_PLAYERS < 5 || NUM_PLAYERS > 10)
+			throw new IndexOutOfBoundsException("Too few players");
+		//there's always a virus and a rogue
+		tempRole.add(VIRUS_ROLE);
+		tempRole.add(ROGUE_ROLE);
+		//add the correct number of rogues
+		if(NUM_PLAYERS > 6)
+			tempRole.add(ROGUE_ROLE);
+		if(NUM_PLAYERS > 8)
+			tempRole.add(ROGUE_ROLE);
+		//add the rest of the players
+		while(tempRole.size() < NUM_PLAYERS)
+			tempRole.add(ANTI_VIRUS_ROLE);
+		//assign randomly
+		Collections.shuffle(tempRole);
+		//fancy java 8 specific hack.
+		//cant convert Integer list to int[] directly,
+		//so uses a stream to read through values,
+		//maps them to ints using auto unboxing,
+		//then converts the mapping to an array
+		//also this comment took more lines than a normal solution
+		//but this line looks cool so it's all good.
+		role = tempRole.stream().mapToInt(i->i).toArray();
 	}
 		
 	private void initDecks()
@@ -63,6 +148,8 @@ public class Model extends GridWorldModel
 		shuffleDiscard();
 	}
 	
+	//helpers -----------------------------------------
+	
 	private void shuffleDiscard()
 	{
 		deck.addAll(discard);
@@ -70,9 +157,37 @@ public class Model extends GridWorldModel
 		Collections.shuffle(deck);
 	}
 	
+	private void updateHand(int ag)
+	{
+		Location loc = getAgPos(ag);
+		//remove any existing cards and add in the current card
+		for(int i=0; i < MAX_HAND; i++)
+		{
+			remove(VIRUS_CARD, loc.x-1+i, loc.y+1);
+			remove(ANTI_VIRUS_CARD, loc.x-1+i, loc.y+1);
+			
+			if(hand[ag].size() > i)
+				set(hand[ag].get(i),loc.x-1+i, loc.y+1);
+		}
+	}
+	
+	private void updateBoardCards()
+	{
+		for(int i = 0; i < numVirusCards; i++)
+			set(VIRUS_CARD, 3+i, 5);
+		for(int i = 0; i < numAntiVirusCards; i++)
+			set(ANTI_VIRUS_CARD, 3+i, 7);
+	}
+	
 	//getters -----------------------------------------
 	
 	public List<Integer> getHand(int ag){ return hand[ag]; }
+	
+	public int getRole(int ag){ return role[ag]; }
+	
+	public int getBoardPower(int numVirus){
+		return numVirus == 0 ? NO_ABILITY : board[numVirus - 1];
+	}
 	
 	public int getKernel(){ return kernelID; }
 	
@@ -117,7 +232,7 @@ public class Model extends GridWorldModel
 	}
 	
 	public boolean drawThree(int ag){
-		if(ag != kernelID)
+		if(ag != kernelID || !hand[ag].isEmpty())
 			return false;
 		
 		for(int i = 0; i < 3; i++){
@@ -125,6 +240,8 @@ public class Model extends GridWorldModel
 				shuffleDiscard();
 			hand[ag].add(deck.remove(0));
 		}
+		
+		updateHand(ag);
 		
 		return true;
 	}
@@ -137,6 +254,9 @@ public class Model extends GridWorldModel
 		hand[schedulerID].addAll(hand[ag]);
 		hand[ag].clear();
 		
+		updateHand(ag);
+		updateHand(schedulerID);
+		
 		return true;
 	}
 	
@@ -147,6 +267,9 @@ public class Model extends GridWorldModel
 		boolean success = hand[ag].remove(Integer.valueOf(VIRUS_CARD));
 		if(success)
 			discard.add(VIRUS_CARD);
+		
+		updateHand(ag);
+		
 		return success;
 	}
 	
@@ -157,6 +280,9 @@ public class Model extends GridWorldModel
 		boolean success = hand[ag].remove(Integer.valueOf(ANTI_VIRUS_CARD));
 		if(success)
 			discard.add(ANTI_VIRUS_CARD);
+		
+		updateHand(ag);
+		
 		return success;
 	}
 	
@@ -165,8 +291,12 @@ public class Model extends GridWorldModel
 		//using big Integer here allows us
 		//to call remove(Object) instead of remove(int)
 		boolean success = hand[ag].remove(Integer.valueOf(VIRUS_CARD));
-		if(success)
+		if(success){
 			numVirusCards++;
+			updateBoardCards();
+			updateHand(ag);
+		}
+		
 		return success;
 	}
 	
@@ -175,8 +305,12 @@ public class Model extends GridWorldModel
 		//using big Integer here allows us
 		//to call remove(Object) instead of remove(int)
 		boolean success = hand[ag].remove(Integer.valueOf(ANTI_VIRUS_CARD));
-		if(success)
+		if(success){
 			numAntiVirusCards++;
+			updateBoardCards();
+			updateHand(ag);
+		}
+		
 		return success;	
 	}
 }
--- src/java/View.java
+++ src/java/View.java
@@ -5,6 +5,10 @@ import java.awt.*;
 
 public class View extends GridWorldView
 {
+	private static final Color VIRUS_COLOR = new Color(188, 15, 15);
+	private static final Color ROGUE_COLOR = new Color(244, 66, 66);
+	private static final Color ANTI_VIRUS_COLOR = new Color(66, 134, 244);
+	private static final Color TABLE_COLOR = new Color(188, 99, 43);
 	
 	Model model;
 	
@@ -18,8 +22,47 @@ public class View extends GridWorldView
 	}
 	
 	@Override
-	public void draw(Graphics g, int x , int y, int object)
-	{
+	public void update(int x, int y){
+		//to fix drawing bugs, updates the entire board every update
+		update();
 	}
 	
+	@Override
+	public void draw(Graphics g, int x , int y, int object)
+	{
+		if(object == Model.TABLE){
+			g.setColor(TABLE_COLOR);
+			g.fillRect(x*cellSizeW,y*cellSizeH,cellSizeW-1,cellSizeH-1);
+			g.setColor(TABLE_COLOR.darker());
+			g.drawRect(x*cellSizeW,y*cellSizeH,cellSizeW-1,cellSizeH-1);
+		}
+		if(object == Model.BOARD){
+			g.setColor(Color.GRAY);
+			g.fillRect(x*cellSizeW,y*cellSizeH,cellSizeW-1,cellSizeH-1);
+			g.setColor(Color.DARK_GRAY);
+			g.drawRect(x*cellSizeW,y*cellSizeH,cellSizeW-1,cellSizeH-1);
+		}
+		if(object == Model.VIRUS_CARD){
+			g.setColor(ROGUE_COLOR);
+			g.fillRect(x*cellSizeW,y*cellSizeH,cellSizeW-1,cellSizeH-1);
+			g.setColor(ROGUE_COLOR.darker());
+			g.drawRect(x*cellSizeW,y*cellSizeH,cellSizeW-1,cellSizeH-1);
+		}
+		if(object == Model.ANTI_VIRUS_CARD){
+			g.setColor(ANTI_VIRUS_COLOR);
+			g.fillRect(x*cellSizeW,y*cellSizeH,cellSizeW-1,cellSizeH-1);
+			g.setColor(ANTI_VIRUS_COLOR.darker());
+			g.drawRect(x*cellSizeW,y*cellSizeH,cellSizeW-1,cellSizeH-1);
+		}
+	}
+	
+	public void drawAgent(Graphics g, int x, int y, Color c, int id){
+		
+		if(model.getRole(id) == model.VIRUS_ROLE)
+			super.drawAgent(g,x,y,VIRUS_COLOR,id);
+		if(model.getRole(id) == model.ROGUE_ROLE)
+			super.drawAgent(g,x,y,ROGUE_COLOR,id);
+		if(model.getRole(id) == model.ANTI_VIRUS_ROLE)
+			super.drawAgent(g,x,y,ANTI_VIRUS_COLOR,id);
+	}
 }
